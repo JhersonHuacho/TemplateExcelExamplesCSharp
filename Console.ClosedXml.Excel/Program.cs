6cced21352858c67f8c85872951f0851724bc920
Francisco Jherson Huacho Inga <jhersonhuacho@gmail.com>
2019-04-18 08:20:23 -0500

    probando estilos parte 1

--- Console.ClosedXml.Excel/Program.cs
+++ Console.ClosedXml.Excel/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,26 @@ namespace Console.ClosedXml.Excel
     {
         static void Main(string[] args)
         {
+            using (XLWorkbook workbook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workbook.Worksheets.Add("Sample Sheet");
+                worksheet.Cell("A1").Value = "ID";
+                worksheet.Range("A1:A3").Merge();
+                worksheet.Cell("B1").Value = "CodEstudiante";
+                worksheet.Range("B1:B3").Merge();
+                worksheet.Cell("C1").Value = "Nombres";
+                worksheet.Range("C1:C3").Merge();
 
+                worksheet.Range("A1:C3").Style
+                    .Border.SetBottomBorder(XLBorderStyleValues.Thick)
+                    .Border.SetBottomBorderColor(XLColor.Red)
+                    .Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center)
+                    .Font.SetBold(true)
+                    .Fill.SetBackgroundColor(XLColor.Gray);
+
+
+                workbook.SaveAs("HelloWorld.xlsx");
+            }
         }
     }
 }
